366bdb5d0cc17f322f7e588694c9573ea696aae4
Jazib Dawre <jazib980@gmail.com>
2021-06-22 14:42:16 +0530

    Run autocorrect on ocr output

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import argparse
 import subprocess
 
 from path import Path
+from autocorrect import Speller
 
 from utils.split_images import split_lines
 
 
 def main(image_name: str):
+    # Config
     base_file = os.path.join(sys.path[0], "images", image_name)
+    spell = Speller(only_replacements=True)
+
+    # Split Lines
     split_lines(base_file)
 
+    # OCR
     for line_img in glob.glob(os.path.join(os.path.splitext(base_file)[0], "*.png")):
         p = subprocess.run(
             [
@@ -28,6 +34,14 @@ def main(image_name: str):
 
         if p.returncode != 0:
             print(p.stderr)
+
+    # Autocorrect
+    with open(os.path.splitext(base_file)[0] + ".txt", "w") as f:
+        for line_text_file in glob.glob(
+            os.path.join(os.path.splitext(base_file)[0], "*.txt")
+        ):
+            with open(line_text_file, "r") as line_text:
+                f.write(spell(line_text.read()) + "\n")
 
 
 if __name__ == "__main__":
